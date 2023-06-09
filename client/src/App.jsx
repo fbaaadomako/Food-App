db9b9c32c28c9dad3d2b4310a324c45071d07ea9
Felicia Baa-Adomako <feliciab840@gmail.com>
2023-06-09 06:06:55 -0400

    Felicia's test 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>FRIENDLY FOOD FINDER</h1>
       <h1>Vite + React - FELICIA test</h1>
+      <p>Felicia's second test here</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
