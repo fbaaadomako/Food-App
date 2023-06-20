5b0296df1c95aee6b4aa468597cc16ffee9a8bd3
Felicia Baa-Adomako <feliciab840@gmail.com>
2023-06-20 07:13:06 -0400

    React icon installed/Hearts started

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,30 +1,9 @@
-
-import React from 'react';
-import Restaurants from './Restaurants';
-import '../Home.css';
-import logo from '../assets/logo-text.png';
-
-
-
-
-function Home() {
-  return (
-    <div>
-      <div
-      className='logo'>
-        <br/><br/>
-      <img 
-      src= {logo}
-      alt='logo'
-      width={200}
-      height={100} />
-
-
 import React from "react";
 import Restaurants from "./Restaurants";
 import Footer from "./Footer";
 import "./css/Home.css";
 import NavBar from "./NavBar";
+import logo from "../assets/logo-text.png";
 
 function Home() {
   return (
@@ -32,13 +11,14 @@ function Home() {
       <div className="logo">
         <br />
         <br />
+        <img src={logo} alt="logo" width={200} height={100} />
+
         <img
           src="https://www.creativefabrica.com/wp-content/uploads/2018/10/Chef-restaurant-logo-by-DEEMKA-STUDIO-4.jpg"
           alt="logo"
           width={200}
           height={100}
         />
-            
       </div>
       <Restaurants />
       <Footer />
