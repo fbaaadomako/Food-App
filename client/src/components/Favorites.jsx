1809d30e27e3b255e77686b14060ecea20475168
julichow <julichow94@gmail.com>
2023-06-21 22:04:27 +0800

    test run for fetching Places API by restaurant_id

--- client/src/components/Favorites.jsx
+++ client/src/components/Favorites.jsx
@@ -1,11 +1,10 @@
-import React, { useContext } from "react";
-import { useState, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import html2pdf from "html2pdf.js";
 import "./css/restaurants_favorites.css";
 import UserContext from "../context/UserContext";
 
 function Favorites() {
-  const [favRestaurants, setFavRestaurants] = useState([""]);
+  const [favRestaurants, setFavRestaurants] = useState([]);
 
   let auth = useContext(UserContext);
 
@@ -24,7 +23,7 @@ function Favorites() {
       .save();
   };
 
-    useEffect(() => {
+  useEffect(() => {
     requestData();
   }, []);
 
@@ -39,6 +38,43 @@ function Favorites() {
       let data = await results.json();
       console.log(data);
       setFavRestaurants(data.restaurants);
+      fetchRestaurantDetails(data.restaurants);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const fetchRestaurantDetails = async (restaurants) => {
+    try {
+      // const apiKey = "YOUR_GOOGLE_PLACES_API_KEY";
+
+      const updatedRestaurants = await Promise.all(
+        restaurants.map(async (restaurant) => {
+          const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${restaurant.restaurant_id}&fields=name,formatted_address,formatted_phone_number,rating,photos&key=AIzaSyAA5EhIjx8DcDyiaucSKHJNbpqTZr5WKhg`;
+          const response = await fetch(url);
+          const data = await response.json();
+          console.log(data);
+
+          const {
+            name,
+            formatted_address,
+            formatted_phone_number,
+            rating,
+            photos,
+          } = data.result;
+
+          return {
+            ...restaurant,
+            name,
+            formatted_address,
+            formatted_phone_number,
+            rating,
+            photos,
+          };
+        })
+      );
+
+      setFavRestaurants(updatedRestaurants);
     } catch (e) {
       console.log(e);
     }
@@ -46,19 +82,24 @@ function Favorites() {
 
   return (
     <div>
-      {/* Render the content to be converted to PDF */}
       <div id="pdf-container">
         <h1>Here is a list of your favorite restaurants, {auth.user.name}</h1>
         <div>
-        {favRestaurants.map((restaurant) => (
-        <div className="card col-md-4" key={restaurant.id}>
-        <h5 className="card-header">{restaurant.name}</h5>
+          {favRestaurants.map((restaurant) => (
+            <div className="card col-md-4" key={restaurant.id}>
+              <h5 className="card-header">{restaurant.restaurant_id}</h5>
+              <p>Name: {restaurant.name}</p>
+              <p>Address: {restaurant.formatted_address}</p>
+              <p>Phone: {restaurant.formatted_phone_number}</p>
+              <p>Rating: {restaurant.rating}</p>
+              {restaurant.photos &&
+                restaurant.photos.map((photo) => (
+                  <img src={photo} alt="Restaurant" key={photo} />
+                ))}
+            </div>
+          ))}
+        </div>
       </div>
-      ))}
-          </div>
-      </div>
-
-      {/* Button to trigger PDF generation and download */}
       <button onClick={onButtonClick}>Download PDF</button>
     </div>
   );
