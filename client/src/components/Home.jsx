f4bd87c02381651a153e0f09b894b4bc8d4db936
Felicia Baa-Adomako <feliciab840@gmail.com>
2023-06-23 21:28:19 -0400

    React Mapbox installed, markers added statically

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,37 +1,24 @@
-
-import React from 'react';
-import Restaurants from './Restaurants';
+import React from "react";
+import Restaurants from "./Restaurants";
 import "./css/Home.css";
-import logo from '../assets/logo-text.png';
-
-
-
+import logo from "../assets/logo-text.png";
+import Footer from "./Footer";
 
 function Home() {
   return (
     <div>
-      <div
-      className='logo'>
-        <br/><br/>
-      <img 
-      src= {logo}
-      alt='logo'
-      width={200}
-      height={100} />
-
+      <div className="logo">
+        <br />
+        <br />
+        <img src={logo} alt="logo" width={200} height={100} />
       </div>
       <Restaurants />
+      <Footer />
     </div>
   );
 }
 
 export default Home;
-
-
-
-
-
-
 
 /*import React from 'react';
 import Restaurants from './Restaurants';
